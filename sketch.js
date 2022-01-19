046805131ddc273b45f63b450f1105890ff38eeb
cschaf8 <cschaf8@uic.edu>
2022-01-19 10:42:03 -0600

    first sketch

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(5,200,250);
+    fill(250,200,5);
+    strokeWeight(0);
+    circle(100,150,70);
+    
+    fill(155,0,250);
+    strokeWeight(0);
+    circle(175,250,45);
+    circle(135,250,45);
+    
+  }
