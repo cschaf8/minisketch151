1a2fc716c726884218f95eda2e71a2e43f7ee592
cschaf8 <cschaf8@uic.edu>
2022-01-24 14:04:37 -0600

    trying to add color switching

--- sketch.js
+++ sketch.js
@@ -5,8 +5,7 @@ function setup() {
     createCanvas(a, b);
   }
 
-  x = -50;
-  y = -50;
+  r = 153;
 
   function draw() {
     a = windowWidth;
@@ -27,11 +26,38 @@ function setup() {
     fill(140);
     rect(0,b/2,a/2,b/2);
 
-    fill(153, 187, 255);
-    rect(0,b/2,a/16,b/2)
-    rect((2*a)/16,b/2,a/16,b/2)
-    rect((4*a)/16,b/2,a/16,b/2)
-    rect((6*a)/16,b/2,a/16,b/2)
+    //r = 153;
+    // g = 187;
+    // countUp = true;
+    // if(countUp)
+    // {
+    //   ++r;
+    //   if(r > 215)
+    //   {
+    //     console.log("false");
+    //     countUp = false;
+    //   }
+
+    // }
+    // if(countUp == false)
+    // {
+    //   --r;
+    //   console.log("minus");
+    //   if(r < 152)
+    //   {
+    //     countUp = true;
+    //   }
+    // }
+    
+
+    console.log(r);
+    fill(r,187,255);
+
+    //fill(153, 187, 255);
+    rect(0,b/2,a/16,b/2);
+    rect((2*a)/16,b/2,a/16,b/2);
+    rect((4*a)/16,b/2,a/16,b/2);
+    rect((6*a)/16,b/2,a/16,b/2);
 
     rect((9*a)/16,0,a/16,b/2);
     rect((11*a)/16,0,a/16,b/2);
@@ -49,11 +75,36 @@ function setup() {
     rect(0,(4*b)/16,a/2,b/16);
     rect(0,(6*b)/16,a/2,b/16);
 
-    strokeWeight(1);
-    strokeCap(SQUARE);
-    fill(255);
-    rect((6*a)/16,(6*b)/16,a/4,b/4);
+    //strokeWeight(1);
+    //strokeCap(SQUARE);
+    fill(216, 153, 255);
+    rect((6*a)/16,(6*b)/16,a/8,b/4);
+    rect((8*a)/16,(8*b)/16,a/8,b/8);
+    fill(153, 187, 255);
+    rect((6*a)/16,(8*b)/16,a/8,b/8);
+    rect((8*a)/16,(6*b)/16,a/8,b/8);
 
+    fill(220);
+    stroke(220);
+    strokeWeight(1);
+    triangle((6*a)/16,(6*b)/16,(6*a)/16, (8*b)/16, (8*a)/16, (6*b)/16);
+
+    fill(180);
+    stroke(180);
+    strokeWeight(1);
+    triangle((8*a)/16,(6*b)/16,(10*a)/16, (6*b)/16, (10*a)/16, (8*b)/16);
+
+    fill(140);
+    stroke(140);
+    strokeWeight(1);
+    triangle((6*a)/16,(10*b)/16,(6*a)/16, (8*b)/16, (8*a)/16, (10*b)/16);
+
+    fill(100);
+    stroke(100);
+    strokeWeight(1);
+    triangle((10*a)/16,(10*b)/16,(8*a)/16, (10*b)/16, (10*a)/16, (8*b)/16);
+
+    
 
     // fill(153, 187, 255);
     // strokeWeight(1);
